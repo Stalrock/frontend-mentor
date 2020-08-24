f0f07fef7044f10fe1026347b334f07889802f2d
Benjamin Rossin <41836377+Stalroc@users.noreply.github.com>
2020-08-24 21:12:38 +0200

    modif #4 article preview

---
+++ article-preview/assets/js/share.js
@@ -0,0 +1,7 @@
+
+const btnShare = document.getElementById('btn-share');
+const elShare = document.getElementById('article-share');
+
+btnShare.addEventListener('click', () => {
+    elShare.classList.toggle('active');
+})
